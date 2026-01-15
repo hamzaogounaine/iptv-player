56ba4bd9c3bfd016d6cf7e515d86dcc8af8bfae1
hamzaogounaine <hamz2487@gmail.com>
2026-01-15 19:44:54 +0100

    Https and http fixed

--- app/api/stream/route.js
+++ app/api/stream/route.js
@@ -1,8 +1,10 @@
 export async function GET(req) {
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id");
+    const username = process.env.NEXT_PUBLIC_IPTV_USERNAME
+    const password = process.env.NEXT_PUBLIC_IPTV_PASSWORD
   
-    const streamUrl = `http://freeiptv.ottc.xyz:80/live/544657673522/329236004368/${id}.m3u8`;
+    const streamUrl = `http://freeiptv.ottc.xyz:80/live/${username}/${password}/${id}.m3u8`;
   
     const response = await fetch(streamUrl, {
       headers: {
